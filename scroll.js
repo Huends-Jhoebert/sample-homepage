b4088e93bf3fc868314605eb1f0dcc27c56c8b9f
Huends-Jhoebert <huendahuenda20@gmail.com>
2021-04-01 02:07:10 +0800

    initialize project

--- scroll.js
+++ scroll.js
@@ -1,20 +1,13 @@
 
 var mybutton = document.getElementById("myBtn");
-var navBar = document.querySelector('.navbar-2');
-navBar.style.display = 'none';
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-	  navBar.style.display = "flex";
-	  navBar.style.position = 'fixed';
-	  navBar.style.width = '100%';
-	  navBar.style.top = 0;
 	  mybutton.style.display = "block";
-	  navBar.style.backgroundColor = '#fff';
-	} else {
-	  navBar.style.display = "none";
+	} 
+	else {
 	  mybutton.style.display = "none";
 	}
   }
